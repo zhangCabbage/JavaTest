51b6e77ae09a493a3ee10cd13ac9e6048041a129
jiahua <1094006351@qq.com>
2016-05-17 21:06:58 +0800

    做微小修改

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 package zhang.algorithm.JavaTest;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,5 +27,16 @@ public class Test {
 			list.add(o);
 			o = null;
 		}
+		
+		BigInteger one = new BigInteger("1");
+		BigInteger two = new BigInteger("2");
+		one.add(two);
+		System.out.println(one.toString());
+		
+		System.out.println("123"+0);//1230
+		System.out.println((int)'A');//65
+		System.out.println((int)'Z');//90
+		System.out.println((int)'a');//97
+		System.out.println((int)'z');//122
 	}
 }
