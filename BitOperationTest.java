fd1391e3b3ab87bdec5758a68b2b8e35489660d0
jiahua <1094006351@qq.com>
2016-05-28 12:01:16 +0800

    add BitOperationTest

---
+++ BitOperationTest.java
@@ -0,0 +1,12 @@
+package zhang.algorithm.JavaTest;
+
+/**
+ * Created by zhang_zack on 16/5/28.
+ * 用来验证Java中的位运算
+ */
+public class BitOperationTest {
+    public static void main(String[] args) {
+        System.out.println(5&4);//4
+        System.out.println(5&1);//1
+    }
+}
